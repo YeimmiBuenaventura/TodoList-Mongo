3c8f9e0201b168a070d80e7316d5656529f13b6b
Kevin Arias <kariasduarte@gmail.com>
2023-01-20 16:35:02 -0500

    conexion 2.2

--- backend/routes/todoRoutes.js
+++ backend/routes/todoRoutes.js
@@ -8,18 +8,26 @@ router.get("/", (req, res) => {
     });
 });
 
-router.post("/", (req, res) => {
-    Todo.create(req.body, (err, result) => {
-        if(err) throw new Error(err);
-        res.json(result);
+router.post("/", async (req, res) => {
+     router.get("/", (req, res) => {
+        Todo.find((err, result) => {
+            if(err) throw new Error(err);
+            res.json(result);
+        });
     });
+    /* try {
+        const result = await Todo.create(req.body)
+        res.json(result);
+    } catch (error) {
+        throw new Error(err)
+        res.json({ error: true, message: 'ocurrio un error'})
+    } */
 });
 
 router.put("/:id", (req, res) => {
     Todo.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true }, (err, result) => {
         if(err) throw new Error(err);
         res.json(result);
-        console.log("hola")
     });
 });
 
