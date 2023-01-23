206519a372a5f10a554b345972c4e4b1eb6d33a3
YeimmiBuenaventura <yeimmipaola@gmail.com>
2023-01-23 12:19:09 -0500

    Arreglos

--- backend/routes/todoRoutes.js
+++ backend/routes/todoRoutes.js
@@ -9,12 +9,15 @@ router.get("/", (req, res) => {
     });
 });
 //Create a object in JSON when listen POST request
-router.post("/", (req, res) => {
-    Todo.create(req.body, (err, result) => {
-        if(err) throw new Error(err);
+router.post("/", async (req, res) => {
+    try {
+        const result = await Todo.create(req.body)
         res.json(result);
-    });
-});
+    } catch (error) {
+        throw new Error(err)
+        res.json({ error: true, message: 'ocurrio un error'});
+    
+}});
 //Find and update de specific object in the JSON when listen PUT request
 router.put("/:id", (req, res) => {
     Todo.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true }, (err, result) => {
