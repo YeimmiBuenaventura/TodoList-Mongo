02fbc7aea0dc8976b3eeb42215a2ae465adb4f7b
YeimmiBuenaventura <111648722+YeimmiBuenaventura@users.noreply.github.com>
2023-01-25 15:46:44 -0500

    Update index.js

--- app/src/apis/index.js
+++ app/src/apis/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://todo-list-mongo-self.vercel.app"
+    baseURL: "https://todo-list-mongo.vercel.app/"
     //http://localhost:3030
 });
